fecb092747abf4cf4acd6e5a2806ea83a22912d9
Didin Abu Aisy <didin@myworkforce.com>
2017-03-29 08:52:43 +0700

    test

--- config.php
+++ config.php
@@ -1,18 +1,17 @@
-<?php
-	
+<?php	
 	error_reporting(E_ALL);
 
-	$client_id = '56vszWAF';
+	$client_id     = '56vszWAF';
 	$client_secret = 'd08e1ff3d6d3ff345021c2d603dcbd82f374c842';
 	// $client_id = 'testclient';
 	// $client_secret = 'testpass';
 
 	$api = new GoingupApi($client_id,$client_secret);
 
-	if ($_SERVER['SERVER_NAME'] == 'affilify.net') {
-		$api->apiBaseUrl = 'https://affilify.net/api';	
+	if ($_SERVER['SERVER_NAME'] == 'affilify.didin') {
+		$api->apiBaseUrl = 'https://affilify.didin/api';
 	} else {
-		$api->apiBaseUrl = 'https://affilify.didin/api';	
+		$api->apiBaseUrl = 'https://affilify.net/api';				
 	}
 	
 
